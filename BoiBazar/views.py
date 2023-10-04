854a89552c2301b58c53119d4d89ebddd63917da
TheRABU <mdfazlerabbi114@gmail.com>
2023-10-04 20:23:37 +0600

    things added

---
+++ BoiBazar/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homePage(request):
+    return render(request, 'index.html')
+
+def navbar(request):
+    return render(request, 'navbar.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
